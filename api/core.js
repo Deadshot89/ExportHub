6ad449ceb4cccca1155f6c761f44fbdfeab20e04
Deadshot89 <86518220+Deadshot89@users.noreply.github.com>
2026-07-14 22:11:36 +0200

    Delete api/core.js

--- api/core.js
+++
@@ -1,6 +0,0 @@
-import { app } from '@azure/functions';
-import { createPickupService } from '../shared/core.js';
-import { repository, blobs } from '../shared/azure-store.js';
-import { fromResult } from '../shared/http.js';
-const service = createPickupService({ repository, blobs });
-app.http('pickup-pod-file', { methods: ['GET'], authLevel: 'anonymous', route: 'pickup-pod-file', handler: async (request) => fromResult(await service.podFile(request.query.get('token'), request.query.get('file'))) });
