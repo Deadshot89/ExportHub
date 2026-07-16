ea210bef6ac35ba30a6de3ff6ba97fecf09ea7db
Deadshot89 <86518220+Deadshot89@users.noreply.github.com>
2026-07-16 21:28:46 +0200

    RC502

---
+++ api/index (8).js
@@ -0,0 +1,11 @@
+{
+  "name": "exporthub-api",
+  "version": "1.0.0",
+  "private": true,
+  "engines": {
+    "node": ">=18"
+  },
+  "dependencies": {
+    "@azure/storage-blob": "^12.27.0"
+  }
+}
