ea54dc90806f424d40291011bb93bdd5833051e5
Deadshot89 <86518220+Deadshot89@users.noreply.github.com>
2026-09-04 10:40:51 +0200

    RC995 GREEN stage: alten Pickup-Confirm auf sicheren V2-Pfad legen

---
+++ .github/rc995/stage/api/pickup-confirm/index.js
@@ -0,0 +1,3 @@
+'use strict';
+// RC995: Der alte Endpunkt bleibt kompatibel, verwendet aber ausschließlich die neue persönliche-PIN-/Einmal-Link-Logik.
+module.exports=require('../pickup-confirm-v2/index.js');
