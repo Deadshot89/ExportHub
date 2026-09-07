08511032dceb89062e4fca467c7f3ff932777af6
Deadshot89 <86518220+Deadshot89@users.noreply.github.com>
2026-09-07 21:31:34 +0200

    RC1002: Produktionsmarker angleichen

--- production-version.js
+++ production-version.js
@@ -1,2 +1,2 @@
-window.__EXPORTHUB_PRODUCTION_VERSION_PROBE__='RC997';
-// RC1000 QR-Abholung Produktionsdeployment 2026-09-07
+window.__EXPORTHUB_PRODUCTION_VERSION_PROBE__='RC1002';
+// RC1002 gemeinsamer Stand für Produktion, TESTSERVICE und Android 2026-09-07
