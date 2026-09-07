475865d6e16a08951ff50a05d082bedbea19234f
Deadshot89 <86518220+Deadshot89@users.noreply.github.com>
2026-09-07 21:30:52 +0200

    RC1002: Android-Version angleichen

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "de.exporthub.test"
         minSdk = 29
         targetSdk = 36
-        versionCode = 998
-        versionName = "1.0-rc997.1"
+        versionCode = 1002
+        versionName = "1.0-rc1002"
     }
 
     buildFeatures {
