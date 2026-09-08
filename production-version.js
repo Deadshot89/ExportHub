eb8f1ff5ab8daf1f7ac6f154cdc611543c8ac867
Deadshot89 <86518220+Deadshot89@users.noreply.github.com>
2026-09-08 16:11:27 +0200

    RC1009: gemeinsamen Versionsmarker anheben

--- production-version.js
+++ production-version.js
@@ -1,2 +1,2 @@
-window.__EXPORTHUB_PRODUCTION_VERSION_PROBE__='RC1008';
-// RC1008 gemeinsamer Stand für Produktion, TESTSERVICE und Demo 2026-09-08 · 75 reine ExportHUB-System-SOPs mit eigenen Systemgrafiken und externen SOP-Verweisen
+window.__EXPORTHUB_PRODUCTION_VERSION_PROBE__='RC1009';
+// RC1009 gemeinsamer Stand für Produktion, TESTSERVICE und Demo 2026-09-08 · 75 ExportHUB-System-SOPs vollständig auswählbar und mit neuer zweispaltiger Dokumentdarstellung
