97657fa9f7940b65b8e7e3a915eaf3eb4b093457
Deadshot89 <86518220+Deadshot89@users.noreply.github.com>
2026-09-08 18:10:49 +0200

    chore: RC1010 gemeinsamen Versionsmarker setzen

--- production-version.js
+++ production-version.js
@@ -1,2 +1,2 @@
-window.__EXPORTHUB_PRODUCTION_VERSION_PROBE__='RC1009';
-// RC1009 gemeinsamer Stand für Produktion, TESTSERVICE und Demo 2026-09-08 · 75 ExportHUB-System-SOPs vollständig auswählbar und mit neuer zweispaltiger Dokumentdarstellung
+window.__EXPORTHUB_PRODUCTION_VERSION_PROBE__='RC1010';
+// RC1010 gemeinsamer Stand für Produktion, TESTSERVICE und Demo 2026-09-08 · 75 ExportHUB-System-SOPs freigegeben, lesbar und revisionssicher migriert
