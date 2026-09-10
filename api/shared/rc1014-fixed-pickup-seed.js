a6dc7d33b2eedc2ac49810265197d982f16c1cef
Deadshot89 <86518220+Deadshot89@users.noreply.github.com>
2026-09-10 11:18:07 +0200

    RC1024: fehlende feste Abholungen ergänzen

--- api/shared/rc1014-fixed-pickup-seed.js
+++ api/shared/rc1014-fixed-pickup-seed.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const SEED_VERSION = 5;
+const SEED_VERSION = 6;
 const ESSENTRA_COMPANY_KEY = 'essentra';
 const LEGACY_ESSENTRA_COMPANY_KEY = 'legacy-default';
 const SYSTEM_ACTOR = 'System RC1014';
@@ -10,8 +10,21 @@ const ESSENTRA_DEFAULTS = Object.freeze([
   { id:'FIX-RC1014-ESSENTRA-FR-MO', siteLabel:'Frankreich', weekday:1, note:'' },
   { id:'FIX-RC1014-ESSENTRA-IT-MO', siteLabel:'Italien', weekday:1, note:'' },
   { id:'FIX-RC1014-ESSENTRA-OHARE-MO', siteLabel:'O’Hare', weekday:1, note:'' },
+  { id:'FIX-RC1024-ESSENTRA-ESYSCO-MO', siteLabel:'Esysco', weekday:1, note:'' },
+  { id:'FIX-RC1024-ESSENTRA-GAGGENAU-MO', siteLabel:'Gaggenau', weekday:1, note:'' },
+  { id:'FIX-RC1024-ESSENTRA-BARCELONA-MO', siteLabel:'Barcelona', weekday:1, note:'' },
+  { id:'FIX-RC1024-ESSENTRA-MADRID-MO', siteLabel:'Madrid', weekday:1, note:'' },
   { id:'FIX-RC1014-ESSENTRA-FAURECIA-DI', siteLabel:'Faurecia', weekday:2, note:'' },
-  { id:'FIX-RC1014-ESSENTRA-BMP-MI', siteLabel:'BMP', weekday:3, note:'' }
+  { id:'FIX-RC1024-ESSENTRA-WUERTH-DI', siteLabel:'Adolf Würth', weekday:2, note:'Nach Anmelden verschieben' },
+  { id:'FIX-RC1024-ESSENTRA-GORENJE-DI', siteLabel:'Gorenje Slovenien', weekday:2, note:'' },
+  { id:'FIX-RC1014-ESSENTRA-BMP-MI', siteLabel:'BMP', weekday:3, note:'' },
+  { id:'FIX-RC1024-ESSENTRA-BSH-MI', siteLabel:'BSH Hausgeräte', weekday:3, note:'' },
+  { id:'FIX-RC1024-ESSENTRA-CONTITECH-MI', siteLabel:'Contitech', weekday:3, note:'' },
+  { id:'FIX-RC1024-ESSENTRA-WUERTH-DO', siteLabel:'Adolf Würth', weekday:4, note:'' },
+  { id:'FIX-RC1024-ESSENTRA-MADRID-DO', siteLabel:'Madrid', weekday:4, note:'' },
+  { id:'FIX-RC1024-ESSENTRA-BARCELONA-DO', siteLabel:'Barcelona', weekday:4, note:'' },
+  { id:'FIX-RC1024-ESSENTRA-POLEN-DO', siteLabel:'Polen', weekday:4, note:'' },
+  { id:'FIX-RC1024-ESSENTRA-SCHWEDEN-FR', siteLabel:'Schweden', weekday:5, note:'' }
 ]);
 
 const OBSOLETE_V1_IDS = new Set([
