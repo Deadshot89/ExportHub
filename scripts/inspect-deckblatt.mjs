ffcad71778b95c19f495db6ec7e4ae0823b6dc5a
Deadshot89 <86518220+Deadshot89@users.noreply.github.com>
2026-09-15 12:14:05 +0200

    chore: RC1111 Diagnosehelfer entfernen

--- scripts/inspect-deckblatt.mjs
+++
@@ -1,8 +0,0 @@
-import fs from 'node:fs';
-
-const html=fs.readFileSync('index.html','utf8');
-for(const needle of ['/* RC352 Deckblatt */','.rc352-cover{','class="rc352-cover"',"class='rc352-cover'",'rc352-cover-head']){
-  const idx=html.indexOf(needle);
-  console.log('\n=== '+needle+' @ '+idx+' ===\n');
-  if(idx>=0) console.log(html.slice(Math.max(0,idx-800),Math.min(html.length,idx+14000)));
-}
